875ba8ca9bc1268085673d3892741e9cb227b1e6
Thanh Nguyen <lethanh300504@gmail.com>
2025-04-14 23:25:32 +1000

    fix login callback for production

--- src/app/auth/login/route.ts
+++ src/app/auth/login/route.ts
@@ -2,15 +2,20 @@ import { NextResponse } from "next/server"
 import { createClient } from "@/utils/supabase/server"
 import { type Provider } from "@supabase/supabase-js";
 
+function getOriginFromRequest(request: Request) {
+  const proto = request.headers.get("x-forwarded-proto") ?? new URL(request.url).protocol;
+  const host = request.headers.get("x-forwarded-host") ?? new URL(request.url).host;
+  return `${proto}//${host}`;
+}
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const provider =  (searchParams.get("provider") ?? "google") as Provider;
   const supabase = await createClient();
-  const redirectUrl = process.env.NEXT_PUBLIC_SITE_URL 
-    ? `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback` 
-    : process.env.NODE_ENV === "development" 
-      ? "http://localhost:3000/auth/callback" 
-      : new URL("/auth/callback", request.url).origin;
+
+  const origin = getOriginFromRequest(request);
+  const redirectUrl = `${origin}/auth/callback`;
+
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
@@ -23,4 +28,5 @@ export async function GET(request: Request) {
   }
 
   return NextResponse.redirect(data.url)
+
 }
